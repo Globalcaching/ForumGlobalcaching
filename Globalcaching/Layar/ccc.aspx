ff3cc68ec2a403bbbeaf3826becf181062aacbde
Globalcaching <hemenrob@gmail.com>
2014-11-10 21:13:00 +0100

    development

---
+++ Globalcaching/Layar/ccc.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ccc.aspx.cs" Inherits="Globalcaching.ccc" %>
---
+++ Globalcaching/Layar/ccc.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Globalcaching
+{
+    public partial class ccc : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StringBuilder result = new StringBuilder();
+            Response.Clear();
+
+            try
+            {
+                string username = Request.QueryString["usr"];
+                string password = Request.QueryString["pwd"];
+
+                if (username != null && password != null)
+                {
+                    MembershipUser objUsr = Membership.GetUser(username);
+                    if (Membership.ValidateUser(username, password))
+                    {
+                        result.Append("OK");
+                    }
+                    else
+                    {
+                        result.Append("2");
+                    }
+                }
+            }
+            catch
+            {
+                result.Length = 0;
+                result.Append("ERROR");
+            }
+            Response.Write(result.ToString());
+        }
+    }
+}
